591ef0f8dc18f3e095220c53a4d8e4267a367abe
Francisco Rotundo <francisco.rotundo@algoritmospa.com>
2022-12-27 10:49:13 -0300

    Made the logic to add the sample points work, and added some styles to the templates

--- LIMSproject/lims/views.py
+++ LIMSproject/lims/views.py
@@ -11,11 +11,23 @@ from . import models
 def index(request):
     """Index view."""
     clientes = models.Cliente.objects.all()
+    print(clientes)
     if request.method == 'POST':
+        print('cliente' not in request.POST.keys())
         client = request.POST['cliente']
+        puntos = []
+        for valor in request.POST.values():
+           puntos.append(valor)
+        puntos = puntos[2::]
         print(client)
+        # for punto in puntos:
+        #     if punto == "":
+        #         continue
+        #     else:
+        #         models.PuntoDeMuestreo.objects.create(nombre= punto, cliente_id= client) 
     return render(request, 'lims/pruebas.html', {
-        'clientes':clientes
+        'pm':[0,1,2],
+        'clientes': clientes,
     })
 
 @login_required
@@ -41,8 +53,6 @@ def add_client(request):
         actividad = request.POST['actividad']
         models.Cliente.objects.create(titular=titular, rut=rut, direccion=direccion, actividad=actividad)
 
-        
-        
         return redirect('lims:client')
     return render(request, 'lims/add_client.html')
 
@@ -50,23 +60,30 @@ def add_client(request):
 def add_sample_point(request):
     """Add sample point view."""
     clientes = models.Cliente.objects.all()
+    
     if request.method == 'POST':
-        if request.POST['sp-number'] != None:
-            pm = [x for x in range(int(request.POST['sp-number']))]
-            len_pm = len(pm)
-            if len_pm != 1:
-                return render(request, 'lims/add_sample_point.html', {
+        if 'cliente' not in request.POST.keys():
+            if request.POST['sp-number'] != None:
+                pm = [x for x in range(int(request.POST['sp-number']))]
+                len_pm = len(pm)
+                if len_pm != 1:
+                    return render(request, 'lims/add_sample_point.html', {
                     'pm':pm,
                     'len_pm': len_pm,
                     'clientes': clientes,
-                })
-        puntos = []
-        for valor in request.POST.values():
-           puntos.append(valor)
-        puntos = puntos[3::]
-        print(puntos)
-        # for punto in puntos:
-        #     sp = models.PuntoDeMuestreo.objects.create(nombre= (sample_point+str(n)), cliente_id=client.id) 
+                    })
+        else:
+            client = request.POST['cliente']
+            puntos = []
+            for valor in request.POST.values():
+                puntos.append(valor)
+            puntos = puntos[2::]
+            print(puntos)
+            for punto in puntos:
+                if punto == "":
+                    continue
+                else:
+                    models.PuntoDeMuestreo.objects.create(nombre= punto, cliente_id= client) 
     return render(request, 'lims/add_sample_point.html', {
         'pm':[0],
         'clientes': clientes,
